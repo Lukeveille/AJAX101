268282621b368e86d5ba2aed08e1fb1416c9917a
Lukeveille <luke@lukeleveille.com>
2018-04-21 00:51:29 -0400

    Completed AJAX Exercise

--- AJAX101.js
+++ AJAX101.js
@@ -18,11 +18,11 @@ function newAjax() {
 
 function LoadMe() {
     var ajax = newAjax();
-    /*ajax.onreadystatechange = function() {
+    ajax.onreadystatechange = function() {
         if (ajax.readyState == 4 && ajax.status == 200) {
-            document.getElementById('myDiv').innerHTML =    ajax.responseText;
+            document.getElementById('myDiv').innerHTML = ajax.responseText;
         }
-    }*/
+    }
     ajax.open("GET", "http://lukeswebtest.coolpage.biz/mypage.html", true);
     ajax.send();
 }
